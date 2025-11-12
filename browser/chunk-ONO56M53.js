f60b353885370bc7ff9aa67e035c5ecf31213591
vishu843 <145661486+vishu843@users.noreply.github.com>
2025-11-13 02:22:54 +0530

    Auto-generated commit

---
+++ browser/chunk-ONO56M53.js
@@ -0,0 +1,1 @@
+import{Fa as i,K as t,sb as e}from"./chunk-5DY4PJDW.js";import"./chunk-BE352OPC.js";var r=class o{static \u0275fac=function(m){return new(m||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[e]})};export{r as AdminModule};
